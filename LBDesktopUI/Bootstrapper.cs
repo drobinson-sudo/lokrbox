c2c19a9d6ac980611198b36b1c6c4a7fa8bc4460
David Robinson <robinsonda615@gmail.com>
2022-01-02 16:56:28 -0600

    Add Dependency Injection to the WPF project.

--- LBDesktopUI/Bootstrapper.cs
+++ LBDesktopUI/Bootstrapper.cs
@@ -11,14 +11,54 @@ namespace LBDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        //Instantiate Caliburn.Micro's SimpleContainer for dependency injection.
+        private SimpleContainer _container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
         }
 
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
+
+        //This override customizes the start page to ShellView instead of the
+        //default AppWindow.xaml (which I deleted.)
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+
+        //Override GetInstance(), GetAllInstances(), and BuildUp() to return
+        //dependency objects from the SimpleContainer container (IoC).
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
