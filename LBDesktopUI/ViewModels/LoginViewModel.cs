eca8b8e7f7c16600b7b927859e9a0da6f0423825
David Robinson <robinsonda615@gmail.com>
2022-02-07 08:20:19 -0600

    Add an error message to the login form.
    I added a collapsed row to the login form which will show error message from the LogIn() try...catch.

--- LBDesktopUI/ViewModels/LoginViewModel.cs
+++ LBDesktopUI/ViewModels/LoginViewModel.cs
@@ -43,6 +43,40 @@ namespace LBDesktopUI.ViewModels
             }
         }
 
+        private bool _isErrorVisible;
+
+        public bool IsErrorVisible
+        {
+            get 
+            {
+                bool output = false;
+
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+
+                return output; 
+            }
+            
+        }
+
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set 
+            {
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => IsErrorVisible);
+                NotifyOfPropertyChange(() => ErrorMessage);
+                 
+            }
+        }
+
+
+
         // This property is guarding the LogIn button action from being available until it returns true.
         // Caliburn.Micro is taking care of binding this property to the button of the same name (minus 'Can')
         // This confused me until I found the explanation in Caliburn.Micro docs.
@@ -65,11 +99,12 @@ namespace LBDesktopUI.ViewModels
         {
             try
             {
+                ErrorMessage = "";
                 var result = await _apiHelper.Authenticate(UserName, Password);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                ErrorMessage = ex.Message;
             }
         }
 
